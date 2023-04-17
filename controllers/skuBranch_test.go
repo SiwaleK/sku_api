340b41c49b47da348c11efb188daa0b1002f75b6
siwale kamasatjakul <siwale.kama@gmail.com>
2023-04-17 08:53:28 +0700

    comment

--- controllers/skuBranch_test.go
+++ controllers/skuBranch_test.go
@@ -173,51 +173,51 @@ func TestUpdateSkuBranch(t *testing.T) {
 	}
 }
 
-func SkuBranchTestGetAllProduct(t *testing.T) {
-	// Create a mock gin context object
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
+// func SkuBranchTestGetAllProduct(t *testing.T) {
+// 	// Create a mock gin context object
+// 	w := httptest.NewRecorder()
+// 	c, _ := gin.CreateTestContext(w)
 
-	// Create a mock gorm.DB object and set it as the global DB instance
-	mockDB, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("error creating mock database: %v", err)
-	}
-	defer mockDB.Close()
-	initializers.DB, err = gorm.Open(postgres.Open("postgresql://root:secret@localhost:5435/sku_5435?sslmode=disable"), &gorm.Config{})
-	if err != nil {
-		t.Fatalf("error opening mock database connection: %v", err)
-	}
+// 	// Create a mock gorm.DB object and set it as the global DB instance
+// 	mockDB, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("error creating mock database: %v", err)
+// 	}
+// 	defer mockDB.Close()
+// 	initializers.DB, err = gorm.Open(postgres.Open("postgresql://root:secret@localhost:5435/sku_5435?sslmode=disable"), &gorm.Config{})
+// 	if err != nil {
+// 		t.Fatalf("error opening mock database connection: %v", err)
+// 	}
 
-	// Define the expected rows
-	rows := sqlmock.NewRows([]string{"sku_id", "merchant_id", "branch_id", "price", "start_date", "end_date", "is_active"}).
-		AddRow("017b1", "000021", "00002100000", 55.0000, "2023-02-20 09:30:52", nil, 1).
-		AddRow("017b1", "000022", "00002200000", 100.0000, "2023-02-03 10:43:15", nil, 1).
-		AddRow("017b1", "000024", "00002400000", 30.0000, "2023-01-30 03:51:07", nil, 1).
-		AddRow("017b1", "1000000000", "1000000000000", 1024.0000, "2022-10-24 07:23:12", nil, 1).
-		// ('017b1c0a-df2e-4f24-be7c-4ae9cadd24ef','1000000000','1000000000000',9999.0000,'2022-08-29 09:28:01',nil,1),
-		// ('017b1c0a-df2e-4f24-be7c-4ae9cadd24ef','1000000000','1000000000001',299.0000,'2022-11-03 11:10:52',nil,1),
-		// ('01e22ab6-84fe-4ebb-9d9e-b571ac9a6b31','000024','00002400000',159.0000,'2023-03-13 05:10:59',nil,1),
-		// ('02156ce9-2378-4570-b941-90509f2d69a4','000026','00002600000',100.0000,'2023-03-08 07:28:25',nil,1),
-		// ('039e2b17-b557-48ad-8ade-a51495bb4be6','1000000000','1000000000000',11.0000,'2023-01-20 08:04:32',nil,1),
-		// ('0517bf6a-ffe1-4819-9c17-09155cdf8bb5','1000000000','1000000000000',0.0000,'2022-08-31 07:33:33',nil,1);
+// 	// Define the expected rows
+// 	rows := sqlmock.NewRows([]string{"sku_id", "merchant_id", "branch_id", "price", "start_date", "end_date", "is_active"}).
+// 		AddRow("017b1", "000021", "00002100000", 55.0000, "2023-02-20 09:30:52", nil, 1).
+// 		AddRow("017b1", "000022", "00002200000", 100.0000, "2023-02-03 10:43:15", nil, 1).
+// 		AddRow("017b1", "000024", "00002400000", 30.0000, "2023-01-30 03:51:07", nil, 1).
+// 		AddRow("017b1", "1000000000", "1000000000000", 1024.0000, "2022-10-24 07:23:12", nil, 1).
+// 		// ('017b1c0a-df2e-4f24-be7c-4ae9cadd24ef','1000000000','1000000000000',9999.0000,'2022-08-29 09:28:01',nil,1),
+// 		// ('017b1c0a-df2e-4f24-be7c-4ae9cadd24ef','1000000000','1000000000001',299.0000,'2022-11-03 11:10:52',nil,1),
+// 		// ('01e22ab6-84fe-4ebb-9d9e-b571ac9a6b31','000024','00002400000',159.0000,'2023-03-13 05:10:59',nil,1),
+// 		// ('02156ce9-2378-4570-b941-90509f2d69a4','000026','00002600000',100.0000,'2023-03-08 07:28:25',nil,1),
+// 		// ('039e2b17-b557-48ad-8ade-a51495bb4be6','1000000000','1000000000000',11.0000,'2023-01-20 08:04:32',nil,1),
+// 		// ('0517bf6a-ffe1-4819-9c17-09155cdf8bb5','1000000000','1000000000000',0.0000,'2022-08-31 07:33:33',nil,1);
 
-		// Mock the SELECT operation
-		mock.ExpectQuery("^SELECT (.+) FROM skus$").WillReturnRows(rows)
+// 		// Mock the SELECT operation
+// 		mock.ExpectQuery("^SELECT (.+) FROM skus$").WillReturnRows(rows)
 
-	// Call the function with the mock context object
-	SkuBranchGetAllProduct(c)
+// 	// Call the function with the mock context object
+// 	SkuBranchGetAllProduct(c)
 
-	// Verify the response
-	if w.Code != http.StatusOK {
-		t.Errorf("expected status code %d but got %d", http.StatusOK, w.Code)
-	}
-	var sku []models.Sku
-	err = json.Unmarshal(w.Body.Bytes(), &sku)
-	if err != nil {
-		t.Errorf("error unmarshalling response: %v", err)
-	}
-	if len(sku) != 8 {
-		t.Errorf("expected 2 SKUs but got %d", len(sku))
-	}
-}
+// 	// Verify the response
+// 	if w.Code != http.StatusOK {
+// 		t.Errorf("expected status code %d but got %d", http.StatusOK, w.Code)
+// 	}
+// 	var sku []models.Sku
+// 	err = json.Unmarshal(w.Body.Bytes(), &sku)
+// 	if err != nil {
+// 		t.Errorf("error unmarshalling response: %v", err)
+// 	}
+// 	if len(sku) != 8 {
+// 		t.Errorf("expected 2 SKUs but got %d", len(sku))
+// 	}
+// }
